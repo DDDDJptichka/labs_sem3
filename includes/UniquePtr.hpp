537509f615120ead6185671b19fc91f6a3357fbe
Z_maker <daniilmanucaran36964@gmail.com>
2026-09-15 23:25:41 +0300

    UniquePtr realization for arrays

--- includes/UniquePtr.hpp
+++ includes/UniquePtr.hpp
@@ -80,3 +80,77 @@ template <class T> class UniquePtr{
         }
 
 };
+
+template <class T> class UniquePtr<T[]>{
+
+    private:
+
+        T* ptr;
+
+    public:
+
+        UniquePtr(T* p = nullptr) : ptr(p){}
+
+        ~UniquePtr(){
+
+            delete[] ptr;
+
+        }
+
+        UniquePtr(const UniquePtr&) = delete;
+        UniquePtr& operator=(const UniquePtr&) = delete;
+
+        UniquePtr(UniquePtr&& another){
+
+            ptr = another.ptr;
+            another.ptr = nullptr;
+
+        }
+
+        UniquePtr& operator=(UniquePtr&& another){
+
+            if (this != &another){
+
+                delete[] ptr;
+
+                ptr = another.ptr;
+                another.ptr = nullptr;
+
+            }
+
+            return *this;
+
+        }
+
+        T& operator[](size_t index){
+
+            return ptr[index];
+
+        }
+
+        T* get() const{
+
+            return ptr;
+
+        }
+
+        T* release(){
+
+            T* tmp = ptr;
+            ptr = nullptr;
+            return tmp;
+
+        }
+
+        void reset(T* p = nullptr){
+
+            if (ptr != p){
+
+                delete[] ptr;
+                ptr = p;
+
+            }
+
+        }
+
+};
--- tests.cpp
+++ tests.cpp
@@ -15,6 +15,28 @@ TEST(TestUniquePtr, create){
     EXPECT_EQ(b.get(), nullptr);
     EXPECT_EQ(*c, 10);
 
+
+    UniquePtr<int[]> arr(new int [3]);
+
+    arr[0] = 0;
+    arr[1] = 1;
+    arr[2] = 2;
+
+    int* r = arr.get();
+
+    EXPECT_EQ(r[0], 0);
+    EXPECT_EQ(r[1], 1);
+    EXPECT_EQ(r[2], 2);
+
+    UniquePtr<int[]> arrr(new int [2]);
+    arrr[0] = 0;
+    arrr[1] = 1;
+    UniquePtr<int[]> arrrr(std::move(arrr));
+
+    EXPECT_EQ(arrr.get(), nullptr);
+    EXPECT_EQ(arrrr[0], 0);
+    EXPECT_EQ(arrrr[1], 1);
+
 }
 
 TEST(TestUniquePtr, check_methods){
@@ -34,6 +56,30 @@ TEST(TestUniquePtr, check_methods){
     EXPECT_EQ(*b, 10);
 
     delete b;
+
+
+    UniquePtr<int[]> arr(new int [3]);
+    int* ar = new int [2];
+    
+    ar[0] = 0;
+    ar[1] = 1; 
+
+    arr[0] = 10;
+    arr[1] = 11;
+    arr[2] = 12;
+
+    arr.reset(ar);
+
+    EXPECT_EQ(arr[0], 0);
+    EXPECT_EQ(arr[1], 1);
+    
+    int *r = arr.release();
+
+    EXPECT_FALSE(arr.get());
+    EXPECT_EQ(r[0], 0);
+    EXPECT_EQ(r[1], 1);
+
+    delete[] r;
 
 }
 

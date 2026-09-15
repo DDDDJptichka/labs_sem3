ef0e324916aea2dc5504852590886bea6b17a262
Z_maker <daniilmanucaran36964@gmail.com>
2026-09-15 22:43:47 +0300

    + UniquePtr tests of operators

--- tests.cpp
+++ tests.cpp
@@ -36,3 +36,37 @@ TEST(TestUniquePtr, check_methods){
     delete b;
 
 }
+
+TEST(TestUniquePtr, check_operators){
+
+    UniquePtr<int> a(new int (5));
+    UniquePtr<int> b;
+
+    b = std::move(a);
+
+    EXPECT_EQ(*b.get(), 5);
+    EXPECT_EQ(*b, 5);
+    
+    class Test{
+
+        private:
+
+            int val;
+
+        public:
+
+            Test(int value) : val(value){}
+
+            int get(){
+
+                return val;
+
+            }
+
+    };
+
+    UniquePtr<Test> c(new Test (10));
+
+    EXPECT_EQ(c->get(), 10);
+
+}

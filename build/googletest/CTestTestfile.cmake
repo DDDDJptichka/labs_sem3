98998e035a88e0430e7f6adc86088470e36fbd53
Z_maker <daniilmanucaran36964@gmail.com>
2026-09-15 22:45:55 +0300

    ignore build files

--- build/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/User/Desktop/lab_c/sem3_dz1/googletest
-# Build directory: C:/Users/User/Desktop/lab_c/sem3_dz1/build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
